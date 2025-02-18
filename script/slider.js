d8bb68541d61f590d39f2c37c0860b07960a53e4
Nicolas-Tworeck <nico.tworeck@gmail.com>
2025-02-17 23:19:47 -0300

    commit

--- script/slider.js
+++ script/slider.js
@@ -75,3 +75,25 @@ document.addEventListener('DOMContentLoaded', () => {
         setInterval(showNextSlide, 5000);
     });
 });
+
+
+
+//  menu hamburguer
+const hamburger = document.querySelector('.hamburger');
+const menuMobile = document.querySelector('.menu-mobile');
+
+// Abrir ou fechar o menu ao clicar no botão hambúrguer
+hamburger.addEventListener('click', () => {
+    menuMobile.classList.toggle('active');
+});
+
+// Fechar o menu ao clicar fora dele
+document.addEventListener('click', (event) => {
+    const isClickInsideMenu = menuMobile.contains(event.target);
+    const isClickOnHamburger = hamburger.contains(event.target);
+
+    // Fechar o menu apenas se o clique não for no menu nem no botão
+    if (!isClickInsideMenu && !isClickOnHamburger && menuMobile.classList.contains('active')) {
+        menuMobile.classList.remove('active');
+    }
+});
